d4ce36aa48744cc1f8e49beedb7105481648510d
Ilya Petrokas <pashapetrokas@gmail.com>
2023-04-03 10:30:54 +0300

    adder filters+categories

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import ProductList from './components/ProductList/ProductList'
 import Navbar from './components/Navbar/Navbar'
 import { useProductList } from './components/ProductList/useProductList'
+import Filters from './components/FiltersandCategory/Filters'
 
 function App() {
-  const { updateSearch } = useProductList()
+  const { filter, updateFilter, resetFilter, updateSearch } = useProductList()
   return (
     <div>
       <Navbar updateSearch={updateSearch} />
+      <Filters filter={filter} updateFilter={updateFilter} resetFilter={resetFilter} />
       <ProductList />
     </div>
   )
--- src/components/FiltersandCategory/Filters.jsx
+++ src/components/FiltersandCategory/Filters.jsx
@@ -1,7 +1,74 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Filters.css'
-const Filters = () => {
-  return <div></div>
+const Filters = ({ filter, updateFilter, resetFilter }) => {
+  const [categories, setCategories] = useState([])
+  const [selectedCategory, setSelectedCategory] = useState('All')
+
+  useEffect(() => {
+    fetch('/api/category')
+      .then(res => res.json())
+      .then(categories => setCategories(categories))
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+  const handleFilterIsNewUpdater = () => updateFilter({ isNew: !filter.isNew })
+  const handleFilterIsLimitedUpdater = () => updateFilter({ isLimited: !filter.isLimited })
+  const handleCategorySelect = categoryId => (updateFilter({ category: [categoryId] }), setSelectedCategory(categoryId))
+  const handleCategoryAll = () => (resetFilter(), setSelectedCategory('All'))
+  return (
+    <section className="filters">
+      <div className="filters_box">
+        <div className="filters_title">
+          <span className="filters_title_image"></span>
+          <h2 className="filters_title_text">Filters</h2>
+        </div>
+        <div className="filters_unit">
+          <div className="filters_unit_category">
+            <h2 className="filters_unit_category_text">Category</h2>
+            <div className="filters_unit_category_category">
+              <button
+                className={`category_button ${selectedCategory === 'All' ? 'category_button_active' : ''} `}
+                onClick={handleCategoryAll}
+              >
+                All
+              </button>
+              {categories.map(category => (
+                <button
+                  key={category.id}
+                  className={`category_button ${selectedCategory === category.id ? 'category_button_active' : ''}`}
+                  onClick={() => handleCategorySelect(category.id)}
+                >
+                  {category.name}
+                </button>
+              ))}
+            </div>
+          </div>
+          <div className="filters_unit_status">
+            <h2 className="filters_unit_status_text">Status</h2>
+            <div className="filters_unit_status_unit">
+              <input
+                className="status_checkbox"
+                id="is_limited"
+                type="checkbox"
+                onChange={handleFilterIsLimitedUpdater}
+                checked={filter.isLimited}
+              />
+              <label htmlFor="is_limited">Limited</label>
+              <input
+                className="status_checkbox"
+                id="is_new"
+                type="checkbox"
+                onChange={handleFilterIsNewUpdater}
+                checked={filter.isNew}
+              />
+              <label htmlFor="is_new">New</label>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Filters
